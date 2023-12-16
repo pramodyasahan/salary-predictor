791a8a089958252d8ed6d67e65b1cc620728bd65
pramodyasahan <sahandil1013@gmail.com>
2023-12-16 15:32:42 +0530

    feat: plotting the regression line

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
 dataset = pd.read_csv('Salary.csv')
@@ -8,3 +8,10 @@ y = dataset.iloc[:, -1].values
 
 regressor = LinearRegression()
 regressor.fit(X, y)
+
+plt.scatter(X, y, color='red')
+plt.plot(X, regressor.predict(X), color='blue')
+plt.title('Salary vs Experience')
+plt.xlabel('Experience')
+plt.ylabel('Salary')
+plt.show()
