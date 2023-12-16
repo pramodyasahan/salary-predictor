6010cb9169c79882276c0dc768712289c98742c6
pramodyasahan <sahandil1013@gmail.com>
2023-12-16 15:27:19 +0530

    feat: import the dataset

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import pandas as pd
 import matplotlib as plt
 from sklearn.linear_model import LinearRegression
+
+dataset = pd.read_csv('Salary.csv')
+X = dataset.iloc[:, : 1].values
+y = dataset.iloc[:, -1].values
