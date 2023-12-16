979b1e484e948779b06a9e90c8bc81d367bd0c66
pramodyasahan <sahandil1013@gmail.com>
2023-12-16 15:28:08 +0530

    feat: implement the regressor

--- main.py
+++ main.py
@@ -5,3 +5,6 @@ from sklearn.linear_model import LinearRegression
 dataset = pd.read_csv('Salary.csv')
 X = dataset.iloc[:, : 1].values
 y = dataset.iloc[:, -1].values
+
+regressor = LinearRegression()
+regressor.fit(X, y)
